3d4039f9ff69facfb1ce2d87e9d46a2cc527cb19
rbn1993 <surfacerobin@outlook.com>
2019-04-11 01:17:27 +0200

    Page Podcast, isomorphic-fetch and antd added, API audioBoom used

---
+++ pages/podcast.js
@@ -0,0 +1,59 @@
+import { Component } from 'react'
+import 'isomorphic-fetch'
+
+export default class Podcast extends Component {
+  static async getInitialProps() {
+    const req = await fetch('https://api.audioboom.com/channels/recommended')
+    const { body: channels } = await req.json()
+    return { channels }
+  }
+  render() {
+    const { channels } = this.props
+    return (
+      <>
+        <style jsx>{`
+          header {
+            color: #fff;
+            background: #8765ca;
+            padding: 15px;
+            text-align: center;
+          }
+          .channels {
+            display: grid;
+            grid-gap: 15px;
+            padding: 15px;
+            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+          }
+          a.channel {
+            display: block;
+            margin-bottom: 0.5em;
+            color: #333;
+            text-decoration: none;
+          }
+          .channel img {
+            border-radius: 3px;
+            box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+            width: 100%;
+          }
+          h2 {
+            padding: 5px;
+            font-size: 0.9em;
+            font-weight: 600;
+            margin: 0;
+            text-align: center;
+          }
+        `}</style>
+
+        <header>Podcast</header>
+        <div className="channels">
+          {channels.map((channel, index) => (
+            <a className="channel" key={index}>
+              <img src={channel.urls.logo_image.original} alt={channel.title} />
+              <h2>{channel.title}</h2>
+            </a>
+          ))}
+        </div>
+      </>
+    )
+  }
+}
