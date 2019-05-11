1e1188611a4375e841a2e0f29413fed32df71b91
rbn1993 <surfacerobin@outlook.com>
2019-05-11 21:42:12 +0200

    Because of Link bring a page and it is a little slow we could set Link to handleOpenPodcast with hybrid view

--- components/Channel.js
+++ components/Channel.js
@@ -1,7 +1,15 @@
 import { slug } from '../helpers/slug'
 import { Link } from '../routes'
+import { useState, useCallback } from 'react'
 
 const Channel = ({ title, urls, audio_clips, series, channelId }) => {
+  const [podcast, setPodcast] = useState()
+  const handleOpenPodcast = useCallback((e, podcast) => {
+    e.preventDefault()
+    setPodcast({ podcast })
+    console.log({ podcast })
+  }, [])
+
   return (
     <>
       <div
@@ -10,6 +18,7 @@ const Channel = ({ title, urls, audio_clips, series, channelId }) => {
           backgroundImage: `url(${urls.banner_image.original})`
         }}
       />
+      {podcast && <div>holaa podcast</div>}
       <h1 id="channelH1">{title}</h1>
       {/* Si hay series imprimo */}
       {Boolean(series.length) && (
@@ -18,6 +27,7 @@ const Channel = ({ title, urls, audio_clips, series, channelId }) => {
           <div className="channels">
             {series.map((serie, index) => (
               <Link
+                onClick
                 route="channel"
                 params={{ slug: slug(serie.title), id: serie.id }}
                 prefetch
@@ -38,25 +48,30 @@ const Channel = ({ title, urls, audio_clips, series, channelId }) => {
           <h2 id="channelH2">Últimos Podcasts</h2>
           <div className="channels">
             {audio_clips.map((audio, index) => (
-              <Link
-                route="podcast"
-                params={{
-                  slugChannel: slug(title),
-                  idChannel: channelId,
-                  slug: slug(audio.title),
-                  id: audio.id
-                }}
-                prefetch
+              // <Link
+              //   route="podcast"
+              //   params={{
+              //     slugChannel: slug(title),
+              //     idChannel: channelId,
+              //     slug: slug(audio.title),
+              //     id: audio.id
+              //   }}
+              //   prefetch
+              //   key={index}
+              // >
+              <a
+                href={`/${title}.${channelId}/${slug(audio.title)}.${audio.id}`}
+                className="channel"
                 key={index}
+                onClick={e => handleOpenPodcast(e, audio)}
               >
-                <a className="channel" key={index}>
-                  <img src={audio.urls.image} />
-                  <h2 id="channelH2">{audio.title}</h2>
-                  <div className="meta">
-                    {Math.ceil(audio.duration / 60)}:00 min
-                  </div>
-                </a>
-              </Link>
+                <img src={audio.urls.image} />
+                <h2 id="channelH2">{audio.title}</h2>
+                <div className="meta">
+                  {Math.ceil(audio.duration / 60)}:00 min
+                </div>
+              </a>
+              //  </Link>
             ))}
           </div>
         </>
