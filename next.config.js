b1dd4afef0539dd80cd75ed7ba223df981fc5f38
rbn1993 <surfacerobin@outlook.com>
2020-05-25 21:54:08 +0200

    Fix show custom error page in now.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,31 @@
+// next.config.js
+const withOffline = require('next-offline')
+const useCss = require('@zeit/next-css')
+
+const baseConfig = {
+  onDemandEntries: {
+    maxInactivePage: 10 * 60 * 1000,
+    pagesBufferLength: 10
+  }
+}
 /**
  * Para poder utilizar el css personalizado en archivos separados
  * al componente se debe configurar el nextjs
  */
-
-const useCss = require('@zeit/next-css')
 // fix: prevents error when .css files are required by node
 if (typeof require !== 'undefined') {
   require.extensions['.css'] = file => {}
 }
-module.exports = useCss
+
+module.exports = withOffline(
+  useCss({
+    ...baseConfig,
+    exportPathMap: async function (defaultPathMap) {
+      return {
+        ...defaultPathMap,
+        '/404.html': { page: '/_error' },
+        '/': { page: '/' }
+      }
+    }
+  })
+)
