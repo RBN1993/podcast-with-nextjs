4bf6f32ca5478a8f46156b347d0aed9a54d6bbc0
rbn1993 <surfacerobin@outlook.com>
2020-05-28 01:20:26 +0200

    Fix some css and view

--- components/ModalPodcast.js
+++ components/ModalPodcast.js
@@ -1,4 +1,4 @@
-import { Modal, Card } from 'antd'
+import { Modal, Card, Icon } from 'antd'
 
 const Podcast = ({ clip, onClose }) => (
   <Modal
@@ -7,18 +7,22 @@ const Podcast = ({ clip, onClose }) => (
     footer={null}
     centered={true}
     bodyStyle={{ background: ' #8756ca', padding: '0' }}
+    closeIcon={
+      <Icon type='close-circle' theme='twoTone' twoToneColor='#654198' spin />
+    }
+    destroyOnClose
   >
     <Card
-      className="clip"
+      className='clip'
       cover={
         <img src={clip.urls.image || clip.channel.urls.logo_image.original} />
       }
     >
-      <div className="player">
+      <div className='player'>
         <h3>{clip.title}</h3>
         <h6>{clip.channel.title}</h6>
         <audio controls autoPlay={true}>
-          <source src={clip.urls.high_mp3} type="audio/mpeg" />
+          <source src={clip.urls.high_mp3} type='audio/mpeg' />
         </audio>
       </div>
     </Card>
