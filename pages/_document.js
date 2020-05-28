c515ad1c6cd32424803ad009839de902ae1f0698
rbn1993 <surfacerobin@outlook.com>
2020-05-28 02:18:40 +0200

    Pass all requeriments for PWA in Lighthouse (apple-touch-icon added)

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,19 @@ export default class Document extends NextDocument {
             name='description'
             content='A Try To Create Progressive Web App built using Next.js'
           />
-          <meta name='theme-color' content='#ff80cc' />
+          <meta name='theme-color' content='#8756ca' />
+
           <link rel='manifest' href='/static/manifest.json' />
+
+          <link rel='shortcut icon' href='/static/favicon.ico' />
+
+          <link rel='apple-touch-icon' href='/icon.png'></link>
+          <meta name='apple-mobile-web-app-title' content='Podcast' />
+          <meta name='apple-mobile-web-app-capable' content='no' />
+          <meta
+            name='apple-mobile-web-app-status-bar-style'
+            content='black-translucent'
+          />
         </Head>
         <body>
           <Main />
